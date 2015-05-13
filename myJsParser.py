858c432d86d453d0c96f8f5324b8f1dd06601efc
loucq123 <loucq123@gmail.com>
2015-05-13 09:48:32 +0800

    correct the grammar

--- myJsParser.py
+++ myJsParser.py
@@ -1,19 +1,21 @@
 # JavaScript grammar
 #
-# The starting non-terminal is "js" for "JavaScript program" -- which is
+# The starting non-terminal is "program" for "JavaScript program" -- which is
 # just a list of "elements" (to be defined shortly). The parse tree you
 # must return is simply a list containing all of the elements.  
 #
-#       js -> element js
-#       js -> 
+#       program -> topStmts
 #
-# An element is either a function declaration: 
+#       topStmts ->
+#       topStmts -> topStmtsPrefix
 #
-#       element -> FUNCTION IDENTIFIER ( optparams ) compoundstmt
+#       topStmtsPrefix -> topStmt
+#       topStmtsPrefix -> topStmtsPrefix topStmt
 #
-# or a statement following by a semi-colon: 
+#       topStmt -> stmt
+#       topStmt -> functionDefine
 #
-#       element -> stmt ; 
+#       functionDefine -> FUNCTION IDENTIFIER ( optparams ) compoundstmt
 #       
 # The parse tree for the former is the tuple ("function",name,args,body),
 # the parse tree for the latter is the tuple ("stmt",stmt). 
@@ -38,20 +40,20 @@
 #
 # We will consider six kinds of possible statements: 
 #
-#       stmt -> IF exp compoundstmt     
-#       stmt -> IF exp compoundstmt ELSE compoundstmt
-#       stmt -> IDENTIFIER = exp 
-#       stmt -> RETURN exp 
+#       stmt -> IF ( exp ) compoundstmt
+#       stmt -> IF ( exp RPAREN ) ELSE compoundstmt
+#       stmt -> IDENTIFIER = exp ;
+#       stmt -> RETURN exp ;
 #
 # The "if", "assignment" and "return" statements should be familiar. It is
 # also possible to use "var" statements in JavaScript to introduce new
 # local variables (this is not necessary in Python): 
 #
-#       stmt -> VAR IDENTIFIER = exp 
+#       stmt -> VAR IDENTIFIER = exp ;
 #
 # And it is also possible to treat an expression as a statement. This is
 #
-#       stmt -> exp 
+#       stmt -> exp ;
 #
 # The parse trees for statements are all tuples:
 #       ("if-then", conditional, then_branch)
@@ -89,7 +91,7 @@ import myJsLexer                 # use my JavaScript lexer
 from myJsLexer import tokens     # use my JavaScript tokens
 
 
-start = 'js'
+start = 'program'
 
 precedence = (
     ('left', 'OROR'),
@@ -101,108 +103,150 @@ precedence = (
     ('right', 'NOT'),
 ) 
 
-def p_js(p): 
-    'js : element js'
-    p[0] = [p[1]] + p[2]
 
-def p_js_empty(p):
-    'js : '
+def p_program(p):
+    'program : topStmts'
+    p[0] = p[1]
+
+
+def p_topStmts(p):
+    'topStmts : topStmtsPrefix'
+    p[0] = p[1]
+
+
+def p_topStmts_empty(p):
+    'topStmts : '
     p[0] = []
 
-def p_element_function(p):
-    'element : FUNCTION IDENTIFIER LPAREN optparams RPAREN compoundstmt'
+
+def p_topStmtsPrefix_one(p):
+    'topStmtsPrefix : topStmt'
+    p[0] = [p[1]]
+
+
+def p_topStmtsPrefix_more(p):
+    'topStmtsPrefix : topStmtsPrefix topStmt'
+    p[0] = p[1] + [p[2]]
+
+
+def p_topStmt(p):
+    'topStmt : stmt'
+    p[0] = ('stmt', p[1])
+
+
+def p_topStmt_func(p):
+    'topStmt : FUNCTION IDENTIFIER LPAREN optparams RPAREN compoundstmt'
     p[0] = ('function', p[2], p[4], p[6])
 
-def p_element_stmt(p):
-    'element : stmt SEMICOLON'
-    p[0] = ('stmt', p[1])
+
+def p_topStmt_lambda(p):
+    'topStmt : FUNCTION LPAREN optparams RPAREN compoundstmt'
+    p[0] = ('lambda', p[3], p[5])
+
 
 def p_optparams(p):
     'optparams : params'
     p[0] = p[1]
 
+
 def p_optparams_empty(p):
     'optparams : '
     p[0] = []
+
 
 def p_params_one(p):
     'params : IDENTIFIER'
     p[0] = [p[1]]
 
+
 def p_params_multi(p):
     'params : IDENTIFIER COMMA params'
     p[0] = [p[1]] + p[3]
 
+
 def p_compoundstmt(p):
-    'compoundstmt : LBRACE stmts RBRACE'
+    'compoundstmt : LBRACE topStmts RBRACE'
     p[0] = p[2]
 
-def p_statements(p):
-    'stmts : stmt SEMICOLON stmts'
-    p[0] = [p[1]] + p[3]
-
-def p_statements_empty(p):
-    'stmts : '
-    p[0] = []
 
 def p_stmt_if(p):
-    'stmt : IF exp compoundstmt'
-    p[0] = ('if-then', p[2], p[3])
+    'stmt : IF LPAREN exp RPAREN compoundstmt'
+    p[0] = ('if-then', p[3], p[5])
+
 
 def p_stmt_ifelse(p):
-    'stmt : IF exp compoundstmt ELSE compoundstmt'
-    p[0] = ('if-then-else', p[2], p[3], p[5])
+    'stmt : IF LPAREN exp RPAREN compoundstmt ELSE compoundstmt'
+    p[0] = ('if-then-else', p[3], p[5], p[7])
+
 
 def p_stmt_IDENTIFIER(p):
-    'stmt : IDENTIFIER EQUAL exp'
+    'stmt : IDENTIFIER EQUAL exp SEMICOLON'
     p[0] = ('assign', p[1], p[3])
 
+
 def p_stmt_RETURN(p):
-    'stmt : RETURN exp'
+    'stmt : RETURN exp SEMICOLON'
     p[0] = ('return', p[2])
 
+
 def p_stmt_VAR(p):
-    'stmt : VAR IDENTIFIER EQUAL exp'
+    'stmt : VAR IDENTIFIER EQUAL exp SEMICOLON'
     p[0] = ('var', p[2], p[4])
 
+
 def p_stmt_exp(p):
-    'stmt : exp'
+    'stmt : exp SEMICOLON'
     p[0] = ('exp', p[1])
 
 # Here's the rules for simple expressions.
 
+
 def p_exp_identifier(p): 
     'exp : IDENTIFIER'
-    p[0] = ("identifier",p[1]) 
+    p[0] = ("identifier", p[1])
+
+
+def p_exp_minus(p):
+    '''exp : MINUS IDENTIFIER
+           | MINUS exp'''
+    p[0] = ('negative', p[2])
+
 
 def p_exp_number(p):
     'exp : NUMBER'
-    p[0] = ('number',p[1])
+    p[0] = ('number', p[1])
+
 
 def p_exp_string(p):
     'exp : STRING'
-    p[0] = ('string',p[1])
-    
+    p[0] = ('string', p[1])
+
+
 def p_exp_true(p):
     'exp : TRUE'
-    p[0] = ('true','true')
-    
+    p[0] = ('true', 'true')
+
+
 def p_exp_false(p):
     'exp : FALSE'
-    p[0] = ('false','false')
-    
+    p[0] = ('false', 'false')
+
+
 def p_exp_not(p):
     'exp : NOT exp'
     p[0] = ('not', p[2])
+
 
 def p_exp_parens(p):
     'exp : LPAREN exp RPAREN'
     p[0] = p[2]
 
-def p_exp_andand(p):
+
+def p_exp_operation(p):
     '''exp : exp OROR exp
            | exp ANDAND exp
            | exp EQUALEQUAL exp
+           | exp NOTEQUAL exp
            | exp LT exp
            | exp GT exp
            | exp LE exp
@@ -213,23 +257,34 @@ def p_exp_andand(p):
            | exp DIVIDE exp'''
     p[0] = ('binop', p[1], p[2], p[3])
 
-def p_exp_IDENTIFIER(p):
+
+def p_exp_call(p):
     'exp : IDENTIFIER LPAREN optargs RPAREN'
     p[0] = ('call', p[1], p[3])
+
 
 def p_optargs_empty(p):
     'optargs : '
     p[0] = []
 
+
 def p_optargs(p):
     'optargs : args'
     p[0] = p[1]
+
 
 def p_args_multi(p):
     'args : exp COMMA args'
     p[0] = [p[1]] + p[3]
 
+
 def p_args_one(p):
     'args : exp'
     p[0] = [p[1]]
+
+
+def p_error(p):
+    print("Syntax error at token", p.type)
+    # Just discard the token and tell the parser it's okay.
+    yacc.errok()
 
